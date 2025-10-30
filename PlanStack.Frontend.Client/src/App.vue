5dac412d0a6a2a964f52e8a68415c0f16d7d9f7b
PKCPH <patrick@klingaa.dk>
2025-10-30 13:39:57 +0100

    added canvas to draw walls on

--- PlanStack.Frontend.Client/src/App.vue
+++ PlanStack.Frontend.Client/src/App.vue
@@ -1,46 +1,10 @@
-<script setup>
-import { ref, onMounted } from 'vue'
 
-const products = ref({ entities: [] })
-const loading = ref(true)
-const error = ref(null)
-
-onMounted(async () => {
-  try {
-    const response = await fetch('/api/api/components')
-    if (!response.ok) throw new Error('Failed to fetch data')
-    const data = await response.json()
-    products.value = data
-  } catch (err) {
-    error.value = err.message
-  } finally {
-    loading.value = false
-  }
-})
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Canvas from './components/Canvas.vue'
 </script>
 
-<!-- <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script> -->
-
 <template>
-  <div>
-    <h1>Products</h1>
-    <div v-if="loading">Loading...</div>
-    <div v-else-if="error">{{ error }}</div>
-    <ul v-else>
-      <li v-for="item in products.entities" :key="item.model">
-        <h2>{{ item.name }}</h2>
-        <p>Model: {{ item.model }}</p>
-        <p>Price: ${{ item.price }}</p>
-        <p>Square Meters: {{ item.squareMeters }}</p>
-      </li>
-    </ul>
-  </div>
-</template>
-
-<!-- <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
@@ -50,35 +14,6 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <Canvas />
   </main>
-</template> -->
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</template>
