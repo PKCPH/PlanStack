804b7bebef136133813401ca27016f63aa8fa3d0
Jack Nguyen <Jaackie@live.dk>
2025-11-04 08:37:56 +0100

    Fix build error

---
+++ PlanStack.Backend.WebAPI/Mappings/BaseMappings.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using PlanStack.Backend.Database.QueryModels;
+using PlanStack.Backend.WebAPI.Controllers.Resources.Shared;
+
+namespace PlanStack.Backend.WebAPI.Mappings
+{
+    public class BaseMappings : Profile
+    {
+        public BaseMappings()
+        {
+            // Mapping DataModel to Resource
+            CreateMap(typeof(BaseQueryResult<>), typeof(BaseQueryResultResource<>));
+        }
+    }
+}
